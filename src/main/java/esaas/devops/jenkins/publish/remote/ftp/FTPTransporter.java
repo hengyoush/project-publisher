90fae9502ab2a1b59cb5b03473b025280bbca049
hengyoush <hengyoush1@163.com>
2019-03-14 23:45:34 +0800

    修复目录新建失败的问题，增加了事先校验m远程FTP上传目录是否存在的逻辑

--- src/main/java/esaas/devops/jenkins/publish/remote/ftp/FTPTransporter.java
+++ src/main/java/esaas/devops/jenkins/publish/remote/ftp/FTPTransporter.java
@@ -1,11 +1,14 @@
 package esaas.devops.jenkins.publish.remote.ftp;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPFileFilter;
 
 import esaas.devops.jenkins.common.Util;
 import esaas.devops.jenkins.publish.remote.RemoteAddrWrapper;
@@ -17,33 +20,43 @@ import esaas.devops.jenkins.publish.remote.Transporter;
 public class FTPTransporter implements Transporter {
 
     private final FTPClient client;
-    
+
     public FTPTransporter(FTPClient client) {
         this.client = client; // Do not escape this!
     }
 
     @Override
     public void transport(File src, RemoteAddrWrapper remoteAddr) {
-        // 上传 
+        // 上传
         try {
             String appRoot = remoteAddr.getRelativePath();
             upload(client, src, appRoot);
         } catch (IOException e) {
             e.printStackTrace(Util.getLogger());
             throw new RuntimeException(e);
-        } 
+        }
     }
 
-    private void upload(FTPClient client, File uploadFile, String uploadPath) throws IOException {
+    private void upload(FTPClient client, File uploadFile, final String uploadPath) throws IOException {
         try (InputStream in1 = new FileInputStream(uploadFile)) {
             Util.getLogger().println("开始上传文件，上传路径：" + uploadPath + "本地上传文件路径：" + uploadFile.getAbsolutePath());
-            if (client.makeDirectory(uploadPath) &&  // 创建对应version的目录
-            client.changeWorkingDirectory(uploadPath) && 
-            client.storeFile(uploadFile.getName(), in1))  {// 开始上传文件 
-                Util.getLogger().println("上传文件完成");
-                return;
+            // 首先判断目录是否已经存在
+            FTPFile[] listDirectories = 
+                client.listFiles(new File(uploadPath).getParent(), new FTPFileFilter(){
+                    public boolean accept(FTPFile file) {
+                        return file.getName().equals(new File(uploadPath).getName());
+                    }
+                });
+            if (listDirectories.length == 0 && !client.makeDirectory(uploadPath)) {
+                throw new RuntimeException("创建目录失败，目录： " + uploadPath);
             }
-           throw new RuntimeException("上传失败");
+            if (!client.changeWorkingDirectory(uploadPath)) {
+                throw new RuntimeException("切换目录失败，目录： " + uploadPath);
+            }
+            if (!client.storeFile(uploadFile.getName(), in1)) {
+                throw new RuntimeException("上传失败，目录： " + uploadPath);
+            }
+            Util.getLogger().println("上传成功");
         } catch (IOException e) {
             e.printStackTrace(Util.getLogger());
             throw e;
