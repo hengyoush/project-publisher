563e884a63c07fc439fdeba8d3f851b1a4581fdf
hengyoush <hengyoush1@163.com>
2019-03-14 23:46:32 +0800

    将共用逻辑抽象

--- src/main/java/esaas/devops/jenkins/pkg/handler/DefaultLibHandler.java
+++ src/main/java/esaas/devops/jenkins/pkg/handler/DefaultLibHandler.java
@@ -10,8 +10,7 @@ import java.io.IOException;
 
 /**
  * 处理lib目录，需要把jenkins的workspace下的ProjectRoot目录下的lib中的jar包
- * copy到packageRoot下的lib目录,将application.properties和key.properties也拷贝到
- * 指定目录的lib下。
+ * copy到packageRoot下的lib目录,将application.properties和key.properties也拷贝到 指定目录的lib下。
  */
 public class DefaultLibHandler implements DirHandler {
     private static final String APPLICATION_PROPERTIES = "application.properties";
@@ -22,25 +21,30 @@ public class DefaultLibHandler implements DirHandler {
         // 拷贝jar
         // 获取src下lib目录
         String libPath = Util.getLibClassPath(project);
+        String targetPath = Util.getSrcTargetPath(project);
+        FilePath targetFilePath = Util.getFilePathFromPathStr(targetPath);
         FilePath libFilePath = new FilePath(new File(libPath));
         try {
-            FilePath[] srcLibs = libFilePath.list("*jar");
-            for (FilePath srcLib : srcLibs) {
-                String targetPathStr = Util.getTargetDirStr(project, DirType.LIB) +
-                        File.separator +
-                        srcLib.getName();
-                FilePath target = new FilePath(new File(targetPathStr));
-                srcLib.copyTo(target);
-            }
+            copyJarsToLib(project, libFilePath);
+            copyJarsToLib(project, targetFilePath);
             // 拷贝application.properties 和 key.properties
             copyProperties(project, APPLICATION_PROPERTIES);
             copyProperties(project, KEY_PROPERTIES);
         } catch (Exception e) {
-            Util.getLogger().println("LibHandler执行失败，"  + e.getMessage());
+            Util.getLogger().println("LibHandler执行失败，" + e.getMessage());
             e.printStackTrace(Util.getLogger());
             throw new RuntimeException(e);
         }
 
+    }
+
+    private void copyJarsToLib(Project project, FilePath targetFilePath) throws IOException, InterruptedException {
+        FilePath[] targetJars = targetFilePath.list("*jar");
+        for (FilePath srcLib : targetJars) {
+            String targetPathStr = Util.getTargetDirStr(project, DirType.LIB) + File.separator + srcLib.getName();
+            FilePath target = new FilePath(new File(targetPathStr));
+            srcLib.copyTo(target);
+        }
     }
 
     private void copyProperties(Project project, String propertiesFileName) throws IOException, InterruptedException {
